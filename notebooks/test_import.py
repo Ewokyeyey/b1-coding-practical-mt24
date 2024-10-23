ab4fd44db34068c08f6f2c385791a5b94d33f9e3
Eliana Kwok <eliana.kwok@gmail.com>
2024-10-23 16:32:02 +0100

    clean up files

--- notebooks/test_import.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath('uuv_mission'))
-print(sys.path)
-
-import dynamic
-import controller
--- uuv_mission/test_mission_class.py
+++
@@ -1,6 +0,0 @@
-import dynamic
-
-# mission = dynamic.Mission.from_csv('/Users/elianakwok/Documents/Oxford/Course/Year 3/B1/Scientific Coding/b1-coding-practical-mt24/data/mission.csv')
-mission = dynamic.Mission.from_csv('data/mission.csv')
-print(f"Cave Heights: {mission.cave_height}")
-print(f"Cave Depths: {mission.cave_depth}")
